63d8cb8b5c722f422bbfecb926d7f5521db21f8c
chaya <chayavogel1@gmail.com>
2023-08-07 19:19:54 -0400

    cleaning up the code

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ function isPalindrome(word) {
   // Write your algorithm here
 
   // 1. Pass a string as an argument to the function.
-  // 2. Convert the string into an array using the string.split method and assign to a variable "arrayedString".
-  // 3. Reverse "ArrayedString" using the string.reverse method and assign to a variable "reversedArrayedString".
-  // 4. Convert "reversedArrayedString" back into a string using the array.join method and assign to variable "reversedString".
-  // 5. Check if the string argument strictly equals the variable "reversedString" (which is the string but backwards) and return it, expecting a value of "true" if they equal eachother and "false" if they do not.
+  // 2. Using the else if conditionals, check if the word is an empty string. If it is, return false.
+  // 3. Then, check if the word is a string. If it is, then on the word, add three build in js methods one after the other: string.split, array.reverse, and array.join.
+  // 4. Check if the string argument strictly equals the variable "reversedWord" (which is the string but backwards) and return it, expecting a value of "true" if they equal eachother and "false" if they do not.
+  // 6. If the word is not a string, then return false.
 
-  const array = word.split("")
-  const reversedArray = array.reverse()
-  const string = reversedArray.join("")
-  return (word === string)
+  if (word === "") {
+    return false
+  } else if (typeof word === "string") {
+    const reversedWord = word.split("").reverse().join("")
+    return word === reversedWord
+  } else {
+    return false
+  }
 }
 
 /*
@@ -21,7 +25,6 @@ function isPalindrome(word) {
 
 My function will take in a word as an argument and return "true" if it is a palindrome (a word that reads the same both ways) and "false" if it not.
 I will do this by comparing the word given as an argument and the word in its reversed form and seeing if they strictly equal eachothor.
-I will reverse the word by converting it into an array, reversing the array using the built in reverse array method, and then converting that array back into a string.
 
 */
 
@@ -54,6 +57,16 @@ if (require.main === module) {
 
   console.log("Expecting: true");
   console.log("=>", isPalindrome("otto"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome(""));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome(66733));
 }
 
 module.exports = isPalindrome;
